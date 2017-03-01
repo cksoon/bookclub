8ed3fe6acf65412f353247297e5b41a5b6f64d4e
Boon Hui Teow <boon.hui.teow@ni.com>
2017-03-01 16:59:59 +0800

    add.c : argument from command line

--- add.c
+++ add.c
@@ -3,43 +3,35 @@ int substract(int firstNumber,int secondNumber);
 int multiplication(int firstNumber,int secondNumber);
 int division(int firstNumber,int secondNumber);
 
-int main()
+int main(int argc, char *argv[])
 {
-    int firstNumber, secondNumber, sumOfTwoNumbers;
+	if ( argc != 2 ) /* argc should be 2 for correct execution */
+    {
+        /* We print argv[0] assuming it is the program name */
+        printf( "error!!!");
+    }
+    else 
+    {
+    	int firstNumber, secondNumber, sumOfTwoNumbers;
 
-    printf("Enter two integers: ");
+    	firstNumber = (int*)argv[1];
+    	secondNumber = (int*)argv[2];
+    	
+	    // sum of two numbers in stored in variable sumOfTwoNumbers
+    	sumOfTwoNumbers = firstNumber + secondNumber;
 
-    // Two integers entered by user is stored using scanf() function
-    scanf("%d %d", &firstNumber, &secondNumber);
+    	// Displays sum
+    	printf("%d + %d = %d", firstNumber, secondNumber, sumOfTwoNumbers);
 
-    // sum of two numbers in stored in variable sumOfTwoNumbers
-    sumOfTwoNumbers = firstNumber + secondNumber;
+    	//Displays subtraction
+	    printf("%d - %d = %d", firstNumber, secondNumber, substract(firstNumber,secondNumber));
 
-    // Displays sum
-    printf("%d + %d = %d", firstNumber, secondNumber, sumOfTwoNumbers);
+ 	  	//Displays Multiplication
+    	printf("%d * %d = %d", firstNumber, secondNumber, multiplication(firstNumber,secondNumber));
 
-    //Displays subtraction
-    printf("%d - %d = %d", firstNumber, secondNumber, substract(firstNumber,secondNumber));
-
-   //Displays Multiplication
-    printf("%d * %d = %d", firstNumber, secondNumber, multiplication(firstNumber,secondNumber));
-
-   //Displays Division
-    printf("%d / %d = %d", firstNumber, secondNumber, division(firstNumber,secondNumber));
-    return 0;
-}
-
-int substract(int firstNumber,int secondNumber)
-{
-	return (firstNumber-secondNumber);
-}
-
-int multiplication(int firstNumber,int secondNumber)
-{
-	return (firstNumber*secondNumber);
-}
-
-int division(int firstNumber,int secondNumber)
-{
-	return (firstNumber/secondNumber);
+	    //Displays Division
+ 	    printf("%d / %d = %d", firstNumber, secondNumber, division(firstNumber,secondNumber));
+    	return 0;
+    }
+    
 }
