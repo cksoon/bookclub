d4266532ef0fa6a8b0be6b0047f228738a500726
Archer Leong <archer.leong@ni.com>
2017-03-01 16:31:25 +0800

    calculation

--- add.c
+++ add.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+int substract(int firstNumber,int secondNumber);
+int multiplication(int firstNumber,int secondNumber);
+int division(int firstNumber,int secondNumber);
+
 int main()
 {
     int firstNumber, secondNumber, sumOfTwoNumbers;
-    
+
     printf("Enter two integers: ");
 
     // Two integers entered by user is stored using scanf() function
@@ -11,8 +15,31 @@ int main()
     // sum of two numbers in stored in variable sumOfTwoNumbers
     sumOfTwoNumbers = firstNumber + secondNumber;
 
-    // Displays sum      
+    // Displays sum
     printf("%d + %d = %d", firstNumber, secondNumber, sumOfTwoNumbers);
 
+    //Displays subtraction
+    printf("%d - %d = %d", firstNumber, secondNumber, substract(firstNumber,secondNumber));
+
+   //Displays Multiplication
+    printf("%d * %d = %d", firstNumber, secondNumber, multiplication(firstNumber,secondNumber));
+
+   //Displays Division
+    printf("%d / %d = %d", firstNumber, secondNumber, division(firstNumber,secondNumber));
     return 0;
+}
+
+int substract(int firstNumber,int secondNumber)
+{
+	return (firstNumber-secondNumber);
+}
+
+int multiplication(int firstNumber,int secondNumber)
+{
+	return (firstNumber*secondNumber);
+}
+
+int division(int firstNumber,int secondNumber)
+{
+	return (firstNumber/secondNumber);
 }
